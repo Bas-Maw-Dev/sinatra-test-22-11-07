09aa405b40094ac2d8e49c1dd794ffb16297d891
Bas Maw <sebastien.maw@madetech.com>
2022-11-08 10:51:12 +0000

    gov.uk page working

--- sinatra.rb
+++ sinatra.rb
@@ -6,7 +6,12 @@ set :port, 5500
 get '/' do
     erb :index
 end
+
 get '/llama' do
     '<h1> Be Happy! </h1>
     <img src=\'https://4.bp.blogspot.com/-OxdWwnS5WJ4/T65c_Z1hflI/AAAAAAAANL0/MzkD35Cms7k/s400/dalai-lama-laughs.jpeg\' alt=\'Dalai Lama pointing his fingers up as if they were animal ears\'/>'
+end
+
+get '/govdesign' do
+    erb :govdesign
 end
